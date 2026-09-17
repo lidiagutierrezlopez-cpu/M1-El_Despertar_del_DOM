fcefd1e6fbd306a143e87b6b49cef98172072d42
LidiaGL <lidiagutierrezlopez@gmail.com>
2026-09-17 21:43:16 +0200

    Creación Panel lateral y más colores

--- M1.js
+++ M1.js
@@ -1,13 +1,20 @@
 const tablero = document.querySelector('#tablero');
 const botonAgregar = document.querySelector('#botonAgregar');
+const botonReiniciar = document.querySelector('#botonReiniciar');
+const colores = ['#fff59d', '#d2fad4', '#b3e5fc', '#efc3f7', '#fdbfe2'];
 
 tablero.addEventListener('click', borrar);
 botonAgregar.addEventListener('click', agregar);
+botonReiniciar.addEventListener('click', reiniciar);
 
 function agregar(){
      // Creacion de Post-it
      let nota = document.createElement('div');
      nota.classList.add('postit');
+
+     // Color aleatorio
+     let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+     nota.style.backgroundColor = colorAleatorio;
 
      // Contenido del Post-it para escribir
      let contenido = document.createElement('div');
@@ -33,5 +40,12 @@ function agregar(){
 function borrar(evento){
      if(evento.target.classList.contains('botonBorrar')){
           evento.target.parentElement.remove();
+     }
+}
+
+function reiniciar(){
+     let confirmar = confirm('¿Seguro que quieres borrar todos los post-its?');
+     if(confirmar){
+          tablero.innerHTML = '';
      }
 }
