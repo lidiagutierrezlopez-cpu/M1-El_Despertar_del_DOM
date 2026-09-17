26f5bcdd77d1e099cf9f94058f239bcbb3e95029
LidiaGL <lidiagutierrezlopez@gmail.com>
2026-09-17 20:56:59 +0200

    Creacion del HTML y JS
    Creacion del HTML, y creacion del JS para agregar y borrar Post-it

---
+++ M1.js
@@ -0,0 +1,33 @@
+const tablero = document.querySelector('#tablero');
+const botonAgregar = document.querySelector('#botonAgregar');
+
+tablero.addEventListener('click', borrar);
+botonAgregar.addEventListener('click', agregar);
+
+function agregar(){
+     // Creacion de Post-it
+     let nota = document.createElement('div');
+     nota.classList.add('postit');
+     nota.setAttribute('contenteditable', 'true');
+     nota.textContent = 'Escribe aquí...';
+
+     // Rotación aleatoria entre -5 y 5 porciento
+     let angulo = Math.random() * 10 - 5;
+     console.log('Ángulo generado:', angulo);
+     nota.style.transform = 'rotate(' + angulo + 'deg)';
+
+     // Botón para borrar
+     let botonBorrar = document.createElement('button');
+     botonBorrar.classList.add('botonBorrar');
+     botonBorrar.textContent = 'x';
+     botonBorrar.setAttribute('contenteditable', 'false');
+
+     nota.appendChild(botonBorrar);
+     tablero.appendChild(nota);
+}
+
+function borrar(evento){
+     if(evento.target.classList.contains('botonBorrar')){
+          evento.target.parentElement.remove();
+     }
+}
